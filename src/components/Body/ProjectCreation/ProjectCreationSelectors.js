fdb0244939579e8b686d2eb2d44bd54723998f38
vladislav-kruglenia <32334309vlad@gmail.com>
2020-10-23 18:33:41 +0300

    02 - Refactoring: selectors

--- src/components/Body/ProjectCreation/ProjectCreationSelectors.js
+++ src/components/Body/ProjectCreation/ProjectCreationSelectors.js
@@ -1,17 +1,74 @@
 import {createSelector} from "reselect";
 
-//отдает большой массив с данными о направлениях, тарифах и услугах
-let getNamesDirectionsSelector = (state) => {
-    return state.projectCreation.directionsAndTariffs
+let Directions = {
+    // Отдает большой объект со всей информацией
+    getNamesDirectionsSelector(state) {
+        return state.projectCreation.directionsAndTariffs
+    },
+
+    // Отдает массив объектов с информацией о направлениях, в каждом из которых лежит массив с информацией о тарифах
+    getNamesDirectionsFunc(directions, getNamesTariffs) {
+        return directions
+            .map(d => {
+                if (d.selected) {
+                    // пробегаемся по массиву направления рекламы и выдаем массив объектов в которых название тарифа и статус
+                    let namesTariffs = getNamesTariffs(d);
+                    // отдаем наверх массив объектов, в котором массив с тарифами и название направления
+                    return {
+                        nameDirection: d.nameDirection,
+                        idDirection: d.idDirection,
+                        namesTariffs
+                    }
+                }
+                return null
+            })
+            // фильтруем элементы null, потому что метод map возвращает массив той же длинны, что и получает
+            .filter(item => item !== null)
+    },
+
+    // Отдает массив объектов с информацией о тарифах
+    getNamesTariffsForTariffs(direction) {
+        return direction.tariffsNames.map(tNames => (
+            {
+                tariffName: tNames.name,
+                tariffStatus: tNames.selected,
+                tariffId: tNames.id
+            }
+        ));
+    },
+
+    // Отдает массив объектов с информацией о тарифах, в каждом из которых лежит массив объектов с информацией о услугах
+    getNamesTariffsForSelectors(direction) {
+        return direction.tariffsNames.map(tNames => {
+            if (tNames.selected) {
+                // пробегаемся по массиву тарифа и выдаем массив объектов в которых название услуги и статус
+                let namesServices = Directions.getServiceInformationForSelectors(tNames);
+                // отдаем наверх массив объектов, в котором массив с именами услуг и название тарифа
+                return {
+                    tariffName: tNames.name,
+                    tariffStatus: tNames.selected,
+                    tariffId: tNames.id,
+                    namesServices
+                }
+            }
+            return null
+        }).filter(item => item !== null);
+    },
+
+    // Отдает массив объектов объектов с информацией о услугах
+    getServiceInformationForSelectors(tariffName) {
+        return tariffName.services.map(sNames => (
+            {
+                serviceName: sNames.serviceName,
+                serviceStatus: sNames.selected,
+                serviceId: sNames.idService
+            }
+        ));
+    },
 };
 
-// функция отдает название направления и булевый статус(выбран блок или нет)
 
-
-//получить названия Тарифов в зависимости от выбранных направлений
-
-
-export let getNamesDirections = createSelector(getNamesDirectionsSelector, (directions) => {
+export let getNamesDirections = createSelector(Directions.getNamesDirectionsSelector,(directions) => {
     return directions.map(d => (
         {
             nameDirection: d.nameDirection,
@@ -21,70 +78,16 @@ export let getNamesDirections = createSelector(getNamesDirectionsSelector, (dire
     ));
 });
 
-export let getNamesTariffsDependingSelectedDirections = createSelector(getNamesDirectionsSelector,
+export let getNamesTariffsDependingSelectedDirections = createSelector(Directions.getNamesDirectionsSelector,
     (directions) => {
-        return directions
-            .map(d => {
-                if (d.selected) {
-                    // пробегаемся по массиву направления рекламы и выдаем массив объектов в которых название тарифа и статус
-                    let namesTariffs = d.tariffsNames.map(tNames => (
-                        {
-                            tariffName: tNames.name,
-                            tariffStatus: tNames.selected,
-                            tariffId: tNames.id
-                        }
-                    ));
-                    // отдаем наверх массив объектов, в котором массив с тарифами и название направления
-                    return {
-                        nameDirection: d.nameDirection,
-                        idDirection: d.idDirection,
-                        namesTariffs
-                    }
-                }
-                return null
-            })
-            // фильтруем элементы null, потому что метод map возвращает массив той же длинны, что и получает
-            .filter(item => item !== null)
+        return Directions.getNamesDirectionsFunc(directions, Directions.getNamesTariffsForTariffs)
     });
 
-export let getNamesServicesDependingSelectedTariffs = createSelector(getNamesDirectionsSelector,
+export let getNamesServicesDependingSelectedTariffs = createSelector(Directions.getNamesDirectionsSelector,
     (directions) => {
-        return directions
-            .map(d => {
-                if (d.selected) {
-                    // пробегаемся по массиву направления рекламы и выдаем массив объектов в которых название тарифа и статус
-                    let namesTariffs = d.tariffsNames.map(tNames => {
-                        if(tNames.selected){
-                            // пробегаемся по массиву тарифа и выдаем массив объектов в которых название услуги и статус
-                            let namesServices = tNames.services.map(sNames => (
-                                {
-                                    serviceName: sNames.serviceName,
-                                    serviceStatus: sNames.selected,
-                                    serviceId: sNames.idService
-                                }
-                            ));
-                            // отдаем наверх массив объектов, в котором массив с именами услуг и название тарифа
-                            return {
-                                tariffName: tNames.name,
-                                tariffStatus: tNames.selected,
-                                tariffId: tNames.id,
-                                namesServices
-                            }
-                        }
-                        return null
-                    }).filter(item => item !== null);
+        return Directions.getNamesDirectionsFunc(directions, Directions.getNamesTariffsForSelectors)});
 
-                    // отдаем наверх массив объектов, в котором массив с тарифами и название направления
-                    return {
-                        nameDirection: d.nameDirection,
-                        idDirection: d.idDirection,
-                        namesTariffs
-                    }
-                }
-                return null
-            })
-            // фильтруем элементы null, потому что метод map возвращает массив той же длинны, что и получает
-            .filter(item => item !== null)
-    });
+
+
 
 
