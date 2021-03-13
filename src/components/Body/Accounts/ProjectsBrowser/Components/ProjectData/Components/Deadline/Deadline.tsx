3ff611e7cbb112766be175921327eb34417819ad
vladislav-kruglenia <vladkruglenya@yandex.ru>
2021-03-13 15:41:48 +0300

    11 - Client Account - start state - 1

--- src/components/Body/Accounts/ProjectsBrowser/Components/ProjectData/Components/Deadline/Deadline.tsx
+++ src/components/Body/Accounts/ProjectsBrowser/Components/ProjectData/Components/Deadline/Deadline.tsx
@@ -3,6 +3,7 @@ import style from "./Deadline.module.scss";
 
 export let Deadline = () => {
     return <div className={style.Deadline}>
+        fdfdf
         <div className={style.NumberDays}>Количество дней на настройку: 10</div>
         <div className={style.StartDate}>Начали работу: 01.09.2020</div>
         <div className={style.FinishDate}>Завершили работу: 01.09.2020</div>
